6a339efcee80598005cb366ab52074502c973e5f
Manngold <jsw4820@gmail.com>
2019-07-11 20:43:49 +0900

    review and add styles

--- momentum-clone/bg.js
+++ momentum-clone/bg.js
@@ -1,22 +1,22 @@
 const body = document.querySelector("body");
 
-IMG_NUMBER = 3;
+const IMAGE_NUMBER = 3;
 
-const paintImage = imgNumber => {
-    const image = new Image();
-    image.src = `./images/${imgNumber}.jpg`;
-    body.appendChild(image);
-    image.classList.add("bgImage");
+const paintImage = number => {
+    const img = new Image();
+    img.src = `./images/${number}.jpg`;
+    body.appendChild(img);
+    img.classList.add("bgImage");
 };
 
 const getRandom = () => {
-    const number = Math.floor(Math.random() * IMG_NUMBER) + 1;
+    const number = Math.floor(Math.random() * IMAGE_NUMBER) + 1;
     return number;
 };
 
 const bgInit = () => {
-    const randomNumber = getRandom();
-    paintImage(randomNumber);
+    const ranNumber = getRandom();
+    paintImage(ranNumber);
 };
 
 bgInit();
--- momentum-clone/weather.js
+++ momentum-clone/weather.js
@@ -3,24 +3,24 @@ const COORDS = "coords";
 const API_KEY = "dd4921796c965b0ccbba3efc67e8b7f7";
 
 const getWeather = (lat, lng) => {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric
-    `)
+    fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+    )
         .then(response => response.json())
         .then(json => {
-            console.log(json);
             const temperature = Math.floor(json.main.temp),
                 description = json.weather[0].description,
                 place = json.name;
-            weather.innerText = `${place} temperature is ${temperature}℃ and ${description}`;
+
+            weather.innerText = `${place} temperature is ${temperature} and ${description}`;
         });
 };
 
-const saveCoords = coordsObj => {
-    localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+const saveCoords = coords => {
+    localStorage.setItem(COORDS, JSON.stringify(coords));
 };
 
 const handleGeoSuccess = position => {
-    console.log(position);
     const latitude = position.coords.latitude;
     const longitude = position.coords.longitude;
     const coordsObj = {
@@ -30,6 +30,7 @@ const handleGeoSuccess = position => {
     saveCoords(coordsObj);
     getWeather(latitude, longitude);
 };
+
 const handleGeoFailed = error => {
     console.log(error);
 };
