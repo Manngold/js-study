d1df673c388d58d5454899842116aff23bd1a79d
Manngold <jsw4820@gmail.com>
2019-08-06 21:56:40 +0900

    templete setting using pug

--- youtube/app.js
+++ youtube/app.js
@@ -4,17 +4,21 @@ import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
 import routes from "./routes";
-import globalRouter from "./routers/globalRouter"
+import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
+import { localMiddleware } from "./middleware";
 
 const app = express();
 
 app.use(helmet());
+app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
+
+app.use(localMiddleware);
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
--- youtube/routers/videoRouter.js
+++ youtube/routers/videoRouter.js
@@ -4,10 +4,10 @@ import { videoDetail, videos, upload, editVideo, deleteVideo } from "../controll
 
 const videoRouter = express.Router();
 
-videoRouter.get(routes.videoDetail, videoDetail);
-videoRouter.get(routes.videos, videos);
+
 videoRouter.get(routes.upload, upload);
-videoRouter.get(routes.editVideo, editVideo);
 videoRouter.get(routes.deleteVideo, deleteVideo);
+videoRouter.get(routes.editVideo, editVideo);
+videoRouter.get(routes.videoDetail, videoDetail);
 
 export default videoRouter;
